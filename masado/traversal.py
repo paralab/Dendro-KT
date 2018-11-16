a224c2deebbdae7db1f58faed5612529752574b1
Masado Ishii <masado.ishii@gmail.com>
2018-11-16 11:27:35 -0700

    Notes explaining this is not sufficient to count sizes of the closed multiplication tables

--- masado/traversal.py
+++ masado/traversal.py
@@ -2,13 +2,58 @@
 ##
 ## Masado Ishii  --  UofU SoC, 2018-11-15
 
+## 2018-11-16
+## ----------
+##   It is relatively straightforward to enumerate the possible paths for
+## a single level of refinement. It is possible to count the minimum
+## number of orientations contained in each of these paths. My program
+## currently does both of these tasks. However, this is not the same as
+## finding the minimum number of orientations needed overall,
+## since it only captures the complexity of a single level of refinement.
+##
+##   What we are after is the smallest orientation table that is closed
+## under multiplication.
+##
+##   Note that by unique 'orientation' I mean a pair:
+## (region starting node, curve net displacement).
+## Such a pair determines the connectivity of the curve segment and 
+## the volume filled. Different choices in the hyperplane are left flexible.
+##
+##   With that clarification, our problem can be formulated as a graph problem. 
+## The graph to consider is a bipartite graph. On the left is the set
+## of orientations. On the right is a set of groupings of orientations.
+## An edge from an orientation (left) to a grouping (right) means
+## one of the ways to subdivide that orientation is through the grouping on the
+## right. Edges from a grouping (right) to orientations (left) mean
+## the orientations are contained in that grouping. In my program thus far,
+## an initial orientation on the left is chosen, namely (0, +highest_dim),
+## and all groupings which receive an edge from this orientation are explored,
+## and we count the number of orientations contained in each grouping. The
+## larger problem is to find a minimal orientation table that is closed under
+## multiplication. In terms of our graph, we want to select a subset of
+## groupings on the right, subject to two constraints. The closure constraint
+## is: Every orientation in the range of the subset of groupings must have an
+## edge back into the same subset of groupings. 'Range' here means all the
+## orientations which receive an edge from any one in the subset of groupings.
+## The minimization constraint is: The range of the subset of groupings must
+## be minimal.
+##
+
+
+
+
+
 import collections
 import itertools
 
 __DEBUG__=False;
 
 def __main__():
-    (num_solutions, min_num_repl, min_lines, max_num_repl, max_lines) = traverse();
+    ##dim = 2;   ## The answer it gives is exactly 1 possible solution (correct).
+    dim = 3;   ## The answer it gives is 5 possible solutions.
+    ##dim = 4;   ## The answer it gives is 5733 possible solutions.
+    ##dim = 5;   ## Runs forever.
+    (num_solutions, min_num_repl, min_lines, max_num_repl, max_lines) = traverse(dim);
     print("Total # of valid displacement sequences: %d." % num_solutions);
     print("Minimum # of orientations (%d) on lines" % min_num_repl, min_lines);
     print("Maximum # of orientations (%d) on lines" % max_num_repl, max_lines);
@@ -26,10 +71,7 @@ def generate_moves(inregion_pos, outregion_pos, region_disp, dim):
     else:
         return (1 << d for d in range(dim-1,-1,-1) if (1 << d) != region_disp);
 
-def traverse():
-    ##dim = 2;   ## The answer it gives is exactly 1 possible solution (correct).
-    dim = 3;   ## The answer it gives is 5 possible solutions.
-    ##dim = 4;   ## The answer it gives is 5733 possible solutions.
+def traverse(dim):
     my_base_path = generate_base_path(dim);
     my_base_order = (
             [0] + list(itertools.accumulate(my_base_path, lambda x,y : x ^ y)));
@@ -47,6 +89,8 @@ def traverse():
     moves = collections.deque();
 
     ## Count how many orientations we will need.
+    ## NOTE: This does not take into account further iterations, which
+    ##       fill out the multiplication table of orientations.
     inregion_hist = [];
     min_num_repl = None;
     min_lines = [];
@@ -66,7 +110,7 @@ def traverse():
                 if __DEBUG__: print("Applying the final move (0,%d)." % final_move);
                 move_hist.append(final_move);
                 inregion_hist.append(inregion_pos);
-                print("%5d  " % num_solutions, move_hist);
+                ##print("%5d  " % num_solutions, move_hist);   ## Comment out to save on i/o
 
                 ## Keep track of min and max num of orientations,
                 ## which are determined by net displacement and the
