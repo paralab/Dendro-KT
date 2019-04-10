2ec5fcd79a02acad7087800523f1532655f207f9
Masado Ishii <masado.ishii@gmail.com>
2019-04-10 00:25:44 -0600

    New test to isolate distTreeConstruction.

---
+++ airtight/main/tryConstruction.cpp
@@ -0,0 +1,138 @@
+/**
+ * @file tryConstruction.cpp
+ * @author Masado Ishii, University of Utah, School of Computing
+ * @created 2019-04-09
+ * @description Test distTreeConstruction().
+ */
+
+#include "tsort.h"
+#include "octUtils.h"
+#include "treeNode.h"
+#include "dendro.h"
+
+#include <mpi.h>
+
+#include <iostream>
+#include <stdio.h>
+
+struct Parameters
+{
+  unsigned int ptsPerProc;
+  unsigned int maxPtsPerRegion;
+  double loadFlexibility;
+  /// unsigned int endL;
+  /// unsigned int elementOrder;
+};
+
+
+// --------------------------------------------
+template <unsigned int dim>
+void test(const Parameters &pm, MPI_Comm comm);
+// --------------------------------------------
+
+
+//
+// main()
+//
+int main(int argc, char * argv[])
+{
+  MPI_Init(&argc, &argv);
+
+  int rProc, nProc;
+  MPI_Comm comm = MPI_COMM_WORLD;
+  MPI_Comm_rank(comm, &rProc);
+  MPI_Comm_size(comm, &nProc);
+
+  unsigned int dim;
+  Parameters pm;
+
+  // Set up accepted options.
+  enum CmdOptions { progName, opDim, opPtsPerProc, opMaxPtsPerRegion, opLoadFlexibility, NUM_CMD_OPTIONS };
+  const char *cmdOptions[NUM_CMD_OPTIONS] = { argv[0], "dim", "ptsPerProc", "maxPtsPerRegion", "loadFlexibility"};
+  const unsigned int firstOptional = opPtsPerProc;
+  std::array<const char *, NUM_CMD_OPTIONS> argVals;
+  argVals.fill("");
+  for (unsigned int op = 0; op < argc; op++)
+    argVals[op] = argv[op];
+
+  // Check if we have the required arguments.
+  if (argc < firstOptional)
+  {
+    if (!rProc)
+    {
+      std::cerr << "Usage: ";
+      unsigned int op = 0;
+      for (; op < firstOptional; op++)
+        std::cerr << cmdOptions[op] << " ";
+      for (; op < NUM_CMD_OPTIONS; op++)
+        std::cerr << "[" << cmdOptions[op] << "] ";
+      std::cerr << "\n";
+    }
+    exit(1);
+  }
+
+  // Parse arguments.
+  dim = static_cast<unsigned int>(strtol(argVals[1], NULL, 0));
+  pm.ptsPerProc = argc > opPtsPerProc ? strtol(argVals[opPtsPerProc], NULL, 0) : 100;
+  pm.maxPtsPerRegion = argc > opMaxPtsPerRegion ? strtol(argVals[opMaxPtsPerRegion], NULL, 0) : 1;
+  pm.loadFlexibility = argc > opLoadFlexibility ? strtol(argVals[opLoadFlexibility], NULL, 0) : 0.2;
+
+  // Replay arguments.
+  constexpr bool replayArguments = true;
+  if (replayArguments && !rProc)
+  {
+    for (unsigned int op = 1; op < NUM_CMD_OPTIONS; op++)
+      std::cout << cmdOptions[op] << "==" << argVals[op] << " \n";
+    std::cout << "\n";
+  }
+
+  _InitializeHcurve(dim);
+
+  // Convert dimension argument to template parameter.
+  switch(dim)
+  {
+    case 2: test<2>(pm, comm); break;
+    case 3: test<3>(pm, comm); break;
+    case 4: test<4>(pm, comm); break;
+    default:
+      if (!rProc)
+        std::cerr << "Dimension " << dim << " not currently supported.\n";
+  }
+
+  _DestroyHcurve();
+
+  MPI_Finalize();
+
+  return 0;
+}
+
+
+//
+// test()
+//
+template <unsigned int dim>
+void test(const Parameters &pm, MPI_Comm comm)
+{
+  int rProc, nProc;
+  MPI_Comm_rank(comm, &rProc);
+  MPI_Comm_size(comm, &nProc);
+
+  const char * const functionName = "test";
+  fprintf(stderr, "[%d] Running %s() with "
+                  "dim==%u; "
+                  "ptsPerProc==%u; "
+                  "maxPtsPerRegion==%u; "
+                  "loadFlexibility==%f; "  "\n",
+      rProc, functionName,
+      dim,
+      pm.ptsPerProc, pm.maxPtsPerRegion, pm.loadFlexibility);
+
+  using T = unsigned int;
+
+  std::vector<ot::TreeNode<T,dim>> points = ot::getPts<T,dim>(pm.ptsPerProc);
+
+  std::vector<ot::TreeNode<T,dim>> tree;
+  ot::SFC_Tree<T,dim>::distTreeConstruction(points, tree, pm.maxPtsPerRegion, pm.loadFlexibility, comm);
+
+  fprintf(stderr, "[%d] Finished %s().\n", rProc, functionName);
+}
