9ea0ecf362bfbd9843b69e05fc8f3da52c6429af
Masado Ishii <masado.ishii@gmail.com>
2020-01-17 15:09:08 -0700

    Implement matvec() using new MatvecBase

--- FEM/include/matvec.h
+++ FEM/include/matvec.h
@@ -14,6 +14,8 @@
 #include "nsort.h"    // TNPoint
 #include "eleTreeIterator.h"
 
+#include "sfcTreeLoop_matvec.h"
+
 #include<iostream>
 #include<functional>
 
@@ -32,7 +34,8 @@ namespace fem
    * @param coords: Flattened array of coordinate tuples, [xyz][xyz][...]
    */
   template <typename da>
-  using EleOpT = std::function<void(const da *in, da *out, unsigned int ndofs, double *coords, double scale)>;
+  /// using EleOpT = std::function<void(const da *in, da *out, unsigned int ndofs, double *coords, double scale)>;
+  using EleOpT = std::function<void(const da *in, da *out, unsigned int ndofs, const double *coords, double scale)>;
 
     // Declaring the matvec at the top.
     template<typename T,typename TN, typename RE>
@@ -230,8 +233,44 @@ namespace fem
      * @param [in] eleOp: Elemental operator (i.e. elemental matvec)
      * @param [in] refElement: reference element.
      */
+
+    template <typename T, typename TN, typename RE>
+    void matvec(const T* vecIn, T* vecOut, unsigned int ndofs, const TN *coords, unsigned int sz, const TN &partFront, const TN &partBack, EleOpT<T> eleOp, double scale, const RE* refElement)
+    {
+      // Initialize output vector to 0.
+      std::fill(vecOut, vecOut + ndofs*sz, 0);
+
+      using C = typename TN::coordType;  // If not unsigned int, error.
+      constexpr unsigned int dim = TN::coordDim;
+      const unsigned int eleOrder = refElement->getOrder();
+      const unsigned int npe = intPow(eleOrder+1, dim);
+
+      ot::MatvecBase<dim, T> treeloop(sz, ndofs, eleOrder, coords, vecIn, partFront, partBack);
+      std::vector<T> leafResult(ndofs*npe, 0.0);
+
+      while (!treeloop.isFinished())
+      {
+        if (treeloop.isPre() && treeloop.subtreeInfo().isLeaf())
+        {
+          const double * nodeCoordsFlat = treeloop.subtreeInfo().getNodeCoords();
+          const T * nodeValsFlat = treeloop.subtreeInfo().readNodeValsIn();
+
+          eleOp(nodeValsFlat, &(*leafResult.begin()), ndofs, nodeCoordsFlat, scale);
+
+          treeloop.subtreeInfo().overwriteNodeValsOut(&(*leafResult.begin()));
+        }
+
+        treeloop.step();
+      }
+
+      size_t writtenSz = treeloop.finalize(vecOut);
+
+      if (writtenSz == 0)
+        std::cerr << "Warning: matvec() did not write any data! Loop misconfigured?\n";
+    }
+
     template<typename T,typename TN, typename RE>
-    void matvec(const T* vecIn, T* vecOut, unsigned int ndofs, const TN* coords, unsigned int sz, const TN &partFront, const TN &partBack, EleOpT<T> eleOp, double scale, const RE* refElement)
+    void matvec_eleTreeIterator(const T* vecIn, T* vecOut, unsigned int ndofs, const TN* coords, unsigned int sz, const TN &partFront, const TN &partBack, EleOpT<T> eleOp, double scale, const RE* refElement)
     {
       // Initialize output vector to 0.
       std::fill(vecOut, vecOut + ndofs*sz, 0);
