96b0fdcd8c6147419618bc176e0535b955669d5a
Masado Ishii <masado.ishii@gmail.com>
2020-03-30 17:08:03 -0600

    test can create vec and do matvec on multiDA and surrogate multiDA

--- test/testGenerateGridHierarchy.cpp
+++ test/testGenerateGridHierarchy.cpp
@@ -2,11 +2,12 @@
 #include "octUtils.h"
 #include "oda.h"
 
+#include "feMatrix.h"
+
 #include <vector>
 #include <iostream>
 
 #include "hcurvedata.h"
-
 
 
 int main(int argc, char * argv[])
@@ -45,33 +46,36 @@ int main(int argc, char * argv[])
 
   std::cout << "---------------------------------\n";
   std::cout << "Stratum0 (size==" << stratum0.size() << ")\n\n";
-  for (const ot::TreeNode<T, dim> &tn : stratum0)
-  {
-    ot::printtn(tn, eLev);
-    std::cout << "\n";
-  }
-  std::cout << "\n\n";
+  /// for (const ot::TreeNode<T, dim> &tn : stratum0)
+  /// {
+  ///   ot::printtn(tn, eLev);
+  ///   std::cout << "\n";
+  /// }
+  /// std::cout << "\n\n";
 
   /// dtree.generateGridHierarchyUp(true, 3, 0.1, comm);
-  dtree.generateGridHierarchyDown(3, 0.1, comm);
+  ot::DistTree<T, dim> surrogateDTree = dtree.generateGridHierarchyDown(3, 0.1, comm);
 
   const std::vector<ot::TreeNode<T, dim>> &stratum1 = dtree.getTreePartFiltered(1);
 
   std::cout << "---------------------------------\n";
   std::cout << "Stratum1 (size==" << stratum1.size() << ")\n\n";
-  for (const ot::TreeNode<T, dim> &tn : stratum1)
-  {
-    ot::printtn(tn, eLev);
-    std::cout << "\n";
-  }
-  std::cout << "\n\n";
+  /// for (const ot::TreeNode<T, dim> &tn : stratum1)
+  /// {
+  ///   ot::printtn(tn, eLev);
+  ///   std::cout << "\n";
+  /// }
+  /// std::cout << "\n\n";
 
 
   const unsigned int order = 2;
 
-  std::vector<ot::DA<dim>> multiDA;
+  std::vector<ot::DA<dim>> multiDA, surrogateMultiDA;
   ot::DA<dim>::multiLevelDA(multiDA, dtree, comm, order);
-  for (int l = 0; l < multiDA.size(); ++l)
+  ot::DA<dim>::multiLevelDA(surrogateMultiDA, surrogateDTree, comm, order);
+
+  const int numStrata = multiDA.size();
+  for (int l = 0; l < numStrata; ++l)
   {
     fprintf(stdout, "%*s[%2d]---DA[%2d] localNodalSz==%u\n",
         10*rProc, "",
@@ -79,6 +83,54 @@ int main(int argc, char * argv[])
         l,
         (unsigned int) multiDA[l].getLocalNodalSz());
   }
+
+
+  // Try to do a matvec on each level.
+  const int dofs = 1;
+  std::vector<std::vector<double>> vecIn(numStrata), vecOut(numStrata);
+  std::vector<std::vector<double>> surrogateVecIn(numStrata), surrogateVecOut(numStrata);
+  for (int l = 0; l < numStrata; ++l)
+  {
+    multiDA[l].createVector(vecIn[l], false, false, dofs);
+    multiDA[l].createVector(vecOut[l], false, false, dofs);
+
+    surrogateMultiDA[l].createVector(surrogateVecIn[l], false, false, dofs);
+    surrogateMultiDA[l].createVector(surrogateVecOut[l], false, false, dofs);
+  }
+
+  std::cerr << "Finished all createVector().\n";
+
+  class myConcreteFeMatrix : public feMatrix<myConcreteFeMatrix, dim>
+  {
+    using T = myConcreteFeMatrix;
+    public:
+      using feMatrix<T,dim>::feMatrix;
+      virtual void elementalMatVec(const VECType *in, VECType *out, unsigned int ndofs, const double *coords, double scale) override
+      {
+        const int nPe = intPow(order + 1, dim);
+        for (int ii = 0; ii < ndofs; ++ii)
+          out[ii] = in[ii];
+      }
+  };
+
+  std::vector<myConcreteFeMatrix> mats;
+  std::vector<myConcreteFeMatrix> surrogateMats;
+  mats.reserve(numStrata);
+  surrogateMats.reserve(numStrata);
+  for (int l = 0; l < numStrata; ++l)
+    mats.emplace_back(&multiDA[l], dofs);
+  for (int l = 0; l < numStrata; ++l)
+    surrogateMats.emplace_back(&surrogateMultiDA[l], dofs);
+
+  std::cerr << "Finished all construct mat.\n";
+
+  for (int l = 0; l < numStrata; ++l)
+    mats[l].matVec(vecIn[l].data(), vecOut[l].data(), 1.0);
+  for (int l = 0; l < numStrata; ++l)
+    surrogateMats[l].matVec(surrogateVecIn[l].data(), surrogateVecOut[l].data(), 1.0);
+
+  std::cerr << "Finished all matVec().\n";
+
 
   _DestroyHcurve();
 
