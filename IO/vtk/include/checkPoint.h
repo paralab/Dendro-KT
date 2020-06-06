28db80fc13cb42c2b39c6f5202595aac50b0d722
Masado Ishii <masado.ishii@gmail.com>
2020-06-05 19:33:39 -0600

    Updated checkpointing for Dendro-KT, +test example

--- IO/vtk/include/checkPoint.h
+++ IO/vtk/include/checkPoint.h
@@ -1,21 +1,21 @@
 //
 // Created by milinda on 7/12/17.
+// Updated by Masado on 6/5/20.
 /**
 *@author Milinda Fernando
 *School of Computing, University of Utah
-*@brief Contans utility functions to save/load mesh (octree) and variable list defined on the otree.
+*@brief Contans utility functions to save/load octree and variable list defined on the otree.
 */
 //
 
-#ifndef SFCSORTBENCH_CHECKPOINT_H
-#define SFCSORTBENCH_CHECKPOINT_H
+#ifndef DENDRO_KT_CHECKPOINT_H
+#define DENDRO_KT_CHECKPOINT_H
 
-#include "TreeNode.h"
-#include "mesh.h"
-#include "json.hpp"
+#include "treeNode.h"
+#include "oda.h"
 #include <iostream>
 #include <fstream>
-using json = nlohmann::json;
+
 namespace io
 {
 
@@ -32,7 +32,8 @@ namespace io
        * <number of elements><list of elements ...>
       **/
 
-      int writeOctToFile(const char * fName,const ot::TreeNode* pNodes,const unsigned int num);
+      template <typename TNT, unsigned int dim>
+      int writeOctToFile(const char * fName,const ot::TreeNode<TNT, dim>* pNodes,const size_t num);
 
       /**
       *@brief Reads an octree speficied by the file name.
@@ -40,57 +41,30 @@ namespace io
       *param[out] pNodes: TreeNodes read from the specified file.
       **/
 
-      int readOctFromFile(const char * fName,std::vector<ot::TreeNode> & pNodes);
+      template <typename TNT, unsigned int dim>
+      int readOctFromFile(const char * fName,std::vector<ot::TreeNode<TNT, dim>> & pNodes);
 
 
       /**
        * @breif: writes the variable vector to a file in binary format.
        * @param[in] fName: input file name.
-       * @param[in] pMesh: pointer to the mesh which the variable defined on
+       * @param[in] da: pointer to the DA which the variable defined on
        * @param [in] vec: pointer to the begin location of the variable vector.
        * binary file format:
-       * <vecsize><localBegin><localEnd><values..>
+       * <totalNodalSz><localBegin><localEnd><ndofs><values..>
        *
        * */
-      template <typename T>
-      int writeVecToFile(const char * fName, const ot::Mesh* pMesh,const T* vec);
-
-      /**
-       * @breif: writes the variable vectors to a file in binary format.
-       * @param[in] fName: input file name.
-       * @param[in] pMesh: pointer to the mesh which the variable defined on
-       * @param [in] vec: pointer to variable vector
-       * @param [in] numVars: number of variables
-       * binary file format:
-       * <vecsize><localBegin><localEnd><values..>
-       *
-       * */
-      template <typename T>
-      int writeVecToFile(const char * fName, const ot::Mesh* pMesh,const T** vec,const unsigned int numVars);
+      template <typename T, unsigned int dim>
+      int writeVecToFile(const char * fName, const ot::DA<dim>* da,const T* vec, int ndofs, bool isGhosted = false);
 
       /**
        * @breif: reads variable vector from the binary file.
        * @param[in] fName: input file name.
-       * @param[in] pMesh: pointer to the mesh which the variable defined on
+       * @param[in] da: pointer to the DA which the variable defined on
        * @param [out] vec: pointer to the begin location of the variable vector.(assumes memory allocated)
        * */
-      template <typename T>
-      int readVecFromFile(const char * fName, const ot::Mesh* pMesh, T* vec);
-
-
-      /**
-       * @breif: reads variable vectors from the binary file.
-       * @param[in] fName: input file name.
-       * @param[in] pMesh: pointer to the mesh which the variable defined on
-       * @param [in] numVars: number of variables
-       * @param [out] vec: pointer to the begin location of the variable vector.(assumes memory allocated)
-       * */
-      template <typename T>
-      int readVecFromFile(const char * fName, const ot::Mesh* pMesh, T** vec, const unsigned int numVars);
-
-
-
-
+      template <typename T, unsigned int dim>
+      int readVecFromFile(const char * fName, const ot::DA<dim>* da, T* vec, int ndofs, bool isGhosted = false);
 
   } // end of namespace checkpoint.
 
@@ -104,92 +78,138 @@ namespace io
 {
   namespace checkpoint
   {
-      template <typename T>
-      int writeVecToFile(const char * fName, const ot::Mesh* pMesh,const T* vec)
+      template <typename TNT, unsigned int dim>
+      int writeOctToFile(FILE* outfile,const ot::TreeNode<TNT, dim>* pNodes,const size_t num)
       {
-          unsigned int numNodes=pMesh->getNumLocalMeshNodes()+pMesh->getNumPreMeshNodes()+pMesh->getNumPostMeshNodes();
-          unsigned int nLocalBegin=pMesh->getNodeLocalBegin();
-          unsigned int nLocalEnd=pMesh->getNodeLocalEnd();
+          if(outfile==NULL) {std::cout<<"Output file invalid "<<std::endl; return 1;}
+          fwrite(&num,sizeof(unsigned int),1,outfile); // write out the number of elements.
+
+          if(num>0)
+            fwrite(pNodes,sizeof(ot::TreeNode<TNT, dim>),num,outfile);
+
+          return 0;
+      }
+
+      template <typename TNT, unsigned int dim>
+      int writeOctToFile(const char * fName,const ot::TreeNode<TNT, dim>* pNodes,const size_t num)
+      {
+          FILE* outfile = fopen(fName,"w");
+          if(outfile==NULL) {std::cout<<fName<<" file open failed "<<std::endl; return 1;}
+          writeOctToFile(outfile, pNodes, num);
+          fclose(outfile);
+          return 0;
+      }
+
+
+      template <typename TNT, unsigned int dim>
+      int readOctFromFile(FILE* inpfile,std::vector<ot::TreeNode<TNT, dim>> & pNodes)
+      {
+          if(inpfile==NULL) {std::cout<<"Input file invalid "<<std::endl; return 1;}
+          unsigned int num=0;
+          fread(&num,sizeof(unsigned int ),1,inpfile);
+
+          pNodes.resize(0);
+          if(num>0)
+          {
+              pNodes.resize(num);
+              fread(&(*(pNodes.begin())),(sizeof(ot::TreeNode<TNT, dim>)),num,inpfile);
+          }
+
+          return 0;
+      }
+
+      template <typename TNT, unsigned int dim>
+      int readOctFromFile(const char * fName,std::vector<ot::TreeNode<TNT, dim>> & pNodes)
+      {
+          FILE* inpfile = fopen(fName,"r");
+          if(inpfile==NULL) {std::cout<<fName<<" file open failed "<<std::endl; return 1;}
+          readOctFromFile(inpfile, pNodes);
+          fclose(inpfile);
+          return 0;
+      }
+
+
+
+      template <typename T, unsigned int dim>
+      int writeVecToFile(const char * fName, const ot::DA<dim>* da, const T* vec, int ndofs, bool isGhosted)
+      {
+          const size_t daTotalNodalSz   = da->getTotalNodalSz();
+          const size_t daLocalNodalSz   = da->getLocalNodalSz();
+          const size_t daLocalNodeBegin = da->getLocalNodeBegin();
+          const size_t daLocalNodeEnd   = daLocalNodalSz + daLocalNodeBegin;
 
           FILE * outfile=fopen(fName,"w");
-          if(outfile==NULL){std::cout<<fName<<" file open failed "<<std::endl; return  1;}
+          if (outfile == NULL)
+          {
+            std::cout << fName << " file open failed " << std::endl;
+            return  1;
+          }
 
-          fwrite(&numNodes,sizeof(unsigned int ),1,outfile);
-          fwrite(&nLocalBegin,sizeof(unsigned int ),1,outfile);
-          fwrite(&nLocalEnd,sizeof(unsigned int ),1,outfile);
-          if(numNodes>0)
-            fwrite((vec+nLocalBegin),sizeof(T),pMesh->getNumLocalMeshNodes(),outfile);
+          fwrite(&daTotalNodalSz,   sizeof(size_t), 1, outfile);
+          fwrite(&daLocalNodeBegin, sizeof(size_t), 1, outfile);
+          fwrite(&daLocalNodeEnd,   sizeof(size_t), 1, outfile);
+          fwrite(&ndofs,          sizeof(int),    1, outfile);
+
+          const size_t offset = (isGhosted ? daLocalNodeBegin*ndofs : 0);
+
+          if(daLocalNodalSz > 0)
+            fwrite((vec + offset), sizeof(T), daLocalNodalSz*ndofs, outfile);
 
           fclose(outfile);
           return 0;
-
       }
 
 
-      template <typename T>
-      int writeVecToFile(const char * fName, const ot::Mesh* pMesh,const T** vec,const unsigned int numVars)
+
+      template <typename T, unsigned int dim>
+      int readVecFromFile(const char * fName, const ot::DA<dim>* da, T* vec, int ndofs, bool isGhosted)
       {
-          unsigned int numNodes=pMesh->getNumLocalMeshNodes()+pMesh->getNumPreMeshNodes()+pMesh->getNumPostMeshNodes();
-          unsigned int nLocalBegin=pMesh->getNodeLocalBegin();
-          unsigned int nLocalEnd=pMesh->getNodeLocalEnd();
+          const size_t daTotalNodalSz   = da->getTotalNodalSz();
+          const size_t daLocalNodalSz   = da->getLocalNodalSz();
+          const size_t daLocalNodeBegin = da->getLocalNodeBegin();
+          const size_t daLocalNodeEnd   = daLocalNodalSz + daLocalNodeBegin;
 
-          FILE * outfile=fopen(fName,"w");
-          if(outfile==NULL){std::cout<<fName<<" file open failed "<<std::endl; return  1;}
-
-          fwrite(&numNodes,sizeof(unsigned int ),1,outfile);
-          fwrite(&nLocalBegin,sizeof(unsigned int ),1,outfile);
-          fwrite(&nLocalEnd,sizeof(unsigned int ),1,outfile);
-          if(numNodes>0)
-              for(unsigned int i=0;i<numVars;i++)
-              fwrite((vec[i]+nLocalBegin),sizeof(T),pMesh->getNumLocalMeshNodes(),outfile);
-
-          fclose(outfile);
-          return 0;
-
-      }
-
-
-      template <typename T>
-      int readVecFromFile(const char * fName, const ot::Mesh* pMesh,T* vec)
-      {
-
-          unsigned int numNodes,nLocalBegin,nLocalEnd;
+          size_t fTotalNodalSz;
+          size_t fLocalNodalSz;
+          size_t fLocalNodeBegin;
+          size_t fLocalNodeEnd;
+          int fNdofs;
 
           FILE * infile = fopen(fName,"r");
-          if(infile==NULL){std::cout<<fName<<" file open failed "<<std::endl; return  1;}
-          fread(&numNodes,sizeof(unsigned int ),1,infile);
-          fread(&nLocalBegin,sizeof(unsigned int ),1,infile);
-          fread(&nLocalEnd,sizeof(unsigned int ),1,infile);
+          if (infile == NULL)
+          {
+            std::cout << fName << " file open failed " << std::endl;
+            return  1;
+          }
 
-          if(numNodes!=(pMesh->getNumPreMeshNodes()+pMesh->getNumLocalMeshNodes()+pMesh->getNumPostMeshNodes())) {std::cout<<fName<<" file number of total node mismatched with the mesh. "<<std::endl; return 1;}
-          if(nLocalBegin!=pMesh->getNodeLocalBegin()) {std::cout<<fName<<" file local node begin location mismatched with the mesh. "<<std::endl; return 1;}
-          if(nLocalEnd!=pMesh->getNodeLocalEnd()) {std::cout<<fName<<" file local node end location mismatched with the mesh. "<<std::endl; return 1;}
 
-          if(numNodes>0)
-              fread((vec+nLocalBegin),sizeof(T),pMesh->getNumLocalMeshNodes(),infile);
+          fread(&fTotalNodalSz,   sizeof(size_t), 1, infile);
+          fread(&fLocalNodeBegin, sizeof(size_t), 1, infile);
+          fread(&fLocalNodeEnd,   sizeof(size_t), 1, infile);
+          fread(&fNdofs,          sizeof(int),    1, infile);
 
-          fclose(infile);
-          return 0;
-      }
+          if (fTotalNodalSz != daTotalNodalSz)
+            std::cout << fName << " file number of total node mismatched with da." << "\n";
 
-      template <typename T>
-      int readVecFromFile(const char * fName, const ot::Mesh* pMesh, T** vec, const unsigned int numVars)
-      {
-          unsigned int numNodes,nLocalBegin,nLocalEnd;
+          if (fLocalNodeBegin != daLocalNodeBegin)
+            std::cout << fName << " file local node begin location mismatched with da." << "\n";
 
-          FILE * infile = fopen(fName,"r");
-          if(infile==NULL){std::cout<<fName<<" file open failed "<<std::endl; return  1;}
-          fread(&numNodes,sizeof(unsigned int ),1,infile);
-          fread(&nLocalBegin,sizeof(unsigned int ),1,infile);
-          fread(&nLocalEnd,sizeof(unsigned int ),1,infile);
+          if (fLocalNodeEnd != daLocalNodeEnd)
+            std::cout << fName << " file local node end location mismatched with da." << "\n";
 
-          if(numNodes!=(pMesh->getNumPreMeshNodes()+pMesh->getNumLocalMeshNodes()+pMesh->getNumPostMeshNodes())) {std::cout<<fName<<" file number of total node mismatched with the mesh. "<<std::endl; return 1;}
-          if(nLocalBegin!=pMesh->getNodeLocalBegin()) {std::cout<<fName<<" file local node begin location mismatched with the mesh. "<<std::endl; return 1;}
-          if(nLocalEnd!=pMesh->getNodeLocalEnd()) {std::cout<<fName<<" file local node end location mismatched with the mesh. "<<std::endl; return 1;}
+          if (fNdofs != ndofs)
+            std::cout << fName << " file ndofs mismatched with function argument ndofs." << "\n";
 
-          if(numNodes>0)
-              for(unsigned int i=0;i<numVars;i++)
-                fread((vec[i]+nLocalBegin),sizeof(T),pMesh->getNumLocalMeshNodes(),infile);
+          if (fTotalNodalSz   != daTotalNodalSz    ||
+              fLocalNodeBegin != daLocalNodeBegin  ||
+              fLocalNodeEnd   != daLocalNodeEnd    ||
+              fNdofs          != ndofs)
+            return 1;
+
+          const size_t offset = (isGhosted ? daLocalNodeBegin*ndofs : 0);
+
+          if(daLocalNodalSz > 0)
+            fread((vec + offset), sizeof(T), daLocalNodalSz*ndofs, infile);
 
           fclose(infile);
           return 0;
@@ -203,4 +223,4 @@ namespace io
 
 
 
-#endif //SFCSORTBENCH_CHECKPOINT_H
+#endif //DENDRO_KT_CHECKPOINT_H
