b43b29ecdec36a6d81a62909dba7c411954cd050
Masado Ishii <masado.ishii@gmail.com>
2020-10-17 16:02:36 -0600

    Comment out KroneckerProduct<..., false>, add <int> and <unsigned> instantiations

--- FEM/src/tensor_special_double.cpp
+++ FEM/src/tensor_special_double.cpp
@@ -12,9 +12,9 @@ void KroneckerProduct<3, double, true>(unsigned M, const double **A, const doubl
 template
 void KroneckerProduct<4, double, true>(unsigned M, const double **A, const double **in, double **out, unsigned int ndofs);
 
-template
-void KroneckerProduct<2, double, false>(unsigned M, const double **A, const double **in, double **out, unsigned int ndofs);
-template
-void KroneckerProduct<3, double, false>(unsigned M, const double **A, const double **in, double **out, unsigned int ndofs);
-template
-void KroneckerProduct<4, double, false>(unsigned M, const double **A, const double **in, double **out, unsigned int ndofs);
+/// template
+/// void KroneckerProduct<2, double, false>(unsigned M, const double **A, const double **in, double **out, unsigned int ndofs);
+/// template
+/// void KroneckerProduct<3, double, false>(unsigned M, const double **A, const double **in, double **out, unsigned int ndofs);
+/// template
+/// void KroneckerProduct<4, double, false>(unsigned M, const double **A, const double **in, double **out, unsigned int ndofs);
--- FEM/src/tensor_special_longlong.cpp
+++ FEM/src/tensor_special_longlong.cpp
@@ -12,11 +12,11 @@ void KroneckerProduct<3, long long, true>(unsigned M, const long long **A, const
 template
 void KroneckerProduct<4, long long, true>(unsigned M, const long long **A, const long long **in, long long **out, unsigned int ndofs);
 
-template
-void KroneckerProduct<2, long long, false>(unsigned M, const long long **A, const long long **in, long long **out, unsigned int ndofs);
-template
-void KroneckerProduct<3, long long, false>(unsigned M, const long long **A, const long long **in, long long **out, unsigned int ndofs);
-template
-void KroneckerProduct<4, long long, false>(unsigned M, const long long **A, const long long **in, long long **out, unsigned int ndofs);
+/// template
+/// void KroneckerProduct<2, long long, false>(unsigned M, const long long **A, const long long **in, long long **out, unsigned int ndofs);
+/// template
+/// void KroneckerProduct<3, long long, false>(unsigned M, const long long **A, const long long **in, long long **out, unsigned int ndofs);
+/// template
+/// void KroneckerProduct<4, long long, false>(unsigned M, const long long **A, const long long **in, long long **out, unsigned int ndofs);
 
 
