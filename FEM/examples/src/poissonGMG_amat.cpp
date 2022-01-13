a6d0dbeb46cb6089501479385782807ea5255702
Masado Ishii <masado.ishii@gmail.com>
2022-01-13 15:33:09 -0700

    Try idea of propagating Non-geom elements up the grid hierarchy
    This is only sufficient if all boundary elements are marked non-geom.
    Should have a different mechanism to auto select coarse elements with missing fine children.

--- FEM/examples/src/poissonGMG_amat.cpp
+++ FEM/examples/src/poissonGMG_amat.cpp
@@ -76,9 +76,13 @@ LocalVector<DT> ghostWriteOverwrite( const ot::GridWrapper<dim> &grid,
                                      const int ndofs,
                                      const GhostedVector<DT> &ghostedVec);
 
+template <typename C, unsigned int dim>
+std::vector<bool> boundaryFlags(
+    const ot::DistTree<C, dim> &distTree,
+    const int stratum = 0);
 
 ot::LocalSubset<DIM> subsetAll(const GridWrapper &grid);
-/// ot::LocalSubset<DIM> subsetBoundary(const GridWrapper &grid);//future
+ot::LocalSubset<DIM> subsetBoundary(const GridWrapper &grid);
 
 
 
@@ -98,6 +102,111 @@ template <typename NodeT>
 std::ostream & printLocal(const ot::DA<DIM> *da, const NodeT *vec, std::ostream & out = std::cout); 
 template <typename NodeT>
 std::ostream & printGhosted(const ot::DA<DIM> *da, const NodeT *vec, std::ostream & out = std::cout);
+
+
+
+template <int dim = DIM>
+class HybridGridChain
+{
+  protected:
+    // GridWrapper provides simplified access to DistTree and DA.
+    std::vector<GridWrapper> m_grid;
+    std::vector<GridWrapper> m_surrGrid;
+    std::vector<ot::LocalSubset<dim>> m_subsets;
+    std::vector<ot::LocalSubset<dim>> m_surrSubsets;
+
+  public:
+    HybridGridChain(
+        const ot::DistTree<unsigned int, dim> *dtree,
+        const ot::DistTree<unsigned int, dim> *surrDtree,
+        const ot::MultiDA<dim> *multiDA,
+        const ot::MultiDA<dim> *surrMultiDA,
+        size_t numGrids);
+
+    const std::vector<GridWrapper> & grid()     const { return m_grid; }
+    const std::vector<GridWrapper> & surrGrid() const { return m_surrGrid; }
+    const GridWrapper & grid(int s)     const { return grid()[s]; }
+    const GridWrapper & surrGrid(int s) const { return surrGrid()[s]; }
+
+    const ot::LocalSubset<dim> & subset(int s)     const { return m_subsets[s]; }
+    const ot::LocalSubset<dim> & surrSubset(int s) const { return m_surrSubsets[s]; }
+
+    void fineNonGeom(ot::LocalSubset<dim> &&subset);
+
+  protected:
+};
+
+template <int dim>
+HybridGridChain<dim>::HybridGridChain(
+        const ot::DistTree<unsigned int, dim> *dtree,
+        const ot::DistTree<unsigned int, dim> *surrDtree,
+        const ot::MultiDA<dim> *multiDA,
+        const ot::MultiDA<dim> *surrMultiDA,
+        size_t numGrids)
+{
+  for (int s = 0; s < numGrids; ++s)
+  {
+    m_grid.emplace_back(dtree, multiDA, s);
+    m_surrGrid.emplace_back(surrDtree, surrMultiDA, s);
+
+    m_subsets.emplace_back(ot::LocalSubset<dim>::makeNone(grid(s).da()));
+    m_surrSubsets.emplace_back(ot::LocalSubset<dim>::makeNone(surrGrid(s).da()));
+  }
+}
+
+template <int dim>
+void HybridGridChain<dim>::fineNonGeom(ot::LocalSubset<dim> &&subset)
+{
+  m_subsets[0] = subset;
+
+  ot::quadTreeToGnuplot(m_subsets[0].relevantOctList(), 3, "subset_" + std::to_string(0), MPI_COMM_WORLD);
+
+  for (int s = 1; s < m_grid.size(); ++s)
+  {
+    const ot::LocalSubset<dim> &fineSubset = m_subsets[s-1];
+    const ot::LocalSubset<dim> &surrSubset = m_surrSubsets[s];
+    const ot::LocalSubset<dim> &coarseSubset = m_subsets[s];
+    const GridWrapper &surrGrid = m_surrGrid[s];
+    const GridWrapper &coarseGrid = m_grid[s];
+
+    const auto overlaps = [&](size_t s, size_t f) {
+      return surrGrid.octList()[s].isAncestorInclusive(
+          fineSubset.relevantOctList()[f]);
+    };
+
+    std::vector<char> surrIncluded(surrGrid.numElements(), false);
+    std::vector<char> coarseIncluded(coarseGrid.numElements(), false);
+
+    for (size_t fine_i = 0,  surr_i = 0;
+             fine_i < fineSubset.getLocalElementSz()
+         and surr_i < surrIncluded.size(); )
+    {
+      if (overlaps(surr_i, fine_i))
+        surrIncluded[surr_i] = true;
+      while (overlaps(surr_i, fine_i))
+        ++fine_i;
+      ++surr_i;
+    }
+
+    par::shift(
+        coarseGrid.da()->getGlobalComm(),
+        surrIncluded.data(),
+        surrIncluded.size(),
+        surrGrid.da()->getGlobalElementBegin(),
+        coarseIncluded.data(),
+        coarseIncluded.size(),
+        coarseGrid.da()->getGlobalElementBegin(),
+        1);
+
+    m_subsets[s] = ot::LocalSubset<dim>(
+        coarseGrid.da(), &coarseGrid.octList(), coarseIncluded, char(true));
+    m_surrSubsets[s] = ot::LocalSubset<dim>(
+        surrGrid.da(), &surrGrid.octList(), surrIncluded, char(true));
+
+    ot::quadTreeToGnuplot(m_subsets[s].relevantOctList(), 3, "subset_" + std::to_string(s), MPI_COMM_WORLD);
+  }
+}
+
 
 
 //
@@ -161,18 +270,16 @@ int main(int argc, char * argv[])
   DA_t::multiLevelDA(multiDA, dtree, comm, eleOrder, 100, sfc_tol);
   DA_t::multiLevelDA(surrMultiDA, surrogateDTree, comm, eleOrder, 100, sfc_tol);
 
-  // GridWrapper provides simplified access to DistTree and DA.
-  std::vector<GridWrapper> grid, surrGrid;
-  for (int s = 0; s < numGrids; ++s)
-  {
-    grid.emplace_back(&dtree, &multiDA, s);
-    surrGrid.emplace_back(&surrogateDTree, &surrMultiDA, s);
-  }
-  const GridWrapper & fineGrid = grid[0];
+  std::cout << "dtree number of grids == " << dtree.getNumStrata() << "\n";
+  std::cout << "fine grid number of elements == " << multiDA[0].getGlobalElementSz() << "\n";
+
+  HybridGridChain<DIM> chain(
+      &dtree, &surrogateDTree, &multiDA, &surrMultiDA, numGrids);
+
+  const GridWrapper & fineGrid = chain.grid()[0];
   const std::vector<size_t> &localBdryIdx = fineGrid.da()->getBoundaryNodeIndices();
   const std::vector<size_t> &ghostedBdryIdx = fineGrid.da()->getGhostedBoundaryNodeIndices();
   const size_t ghostedBdrySz = ghostedBdryIdx.size();
-
 
   // Exact numerical solution to later evaluate error.
   LocalVector<double> u_exact_vec(fineGrid, singleDof);
@@ -233,7 +340,9 @@ int main(int argc, char * argv[])
   std::vector<aMatFree*> amatFree(numGrids, nullptr);
 
           // Happens to be all for this test. Future: boundary elems.
-  ot::LocalSubset<DIM> subset = subsetAll(fineGrid);
+  chain.fineNonGeom(subsetAll(fineGrid));
+  /// chain.fineNonGeom(subsetBoundary(fineGrid));
+  const ot::LocalSubset<DIM> & subset = chain.subset(0);  // nongeom
 
   // Create the fine-grid aMat directly.
   /// allocAMatMaps(fineGrid, meshMaps[0], const_ptr(ghosted_bdry), singleDof);
@@ -249,29 +358,50 @@ int main(int argc, char * argv[])
   amatFree[0]->finalize();
 
 
-  // (prototype for vcycle)
-  //
-  // Matvec: Ghost read, split, op, unsplit, ghost write
-  GhostedVector<double> u_gh = ghostRead(fineGrid, singleDof, u_vec);
-  GhostedVector<double> v_gh(fineGrid, singleDof);
-  // First subset --
-  std::vector<double> u_sub = ot::gather_ndofs(const_ptr(u_gh), subset, singleDof);
-  std::vector<double> v_sub(u_sub.size(), 0);
-      // matvec(): ghosted=true if created(DA), ghosted=false if created(subset)
-  amatFree[0]->matvec(ptr(v_sub), const_ptr(u_sub), false);
-  ot::scatter_ndofs_accumulate(const_ptr(v_sub), ptr(v_gh), subset, singleDof);
-  // -- If there was a second subset, do the same with it
-  //
-  LocalVector<double> v_vec = ghostWriteAccumulate(fineGrid, singleDof, v_gh);
+
+  // 2-grid method
+  const int nvcycle = 30;
+  for (int cycle = 0; cycle < nvcycle; ++cycle)
+  {
+    printf("[cycle=%2d]  |error|=%.2e\n",
+        cycle, sol_err_linf(u_vec));
+
+    // (prototype for vcycle)
+    //
+    // Matvec: Ghost read, split, op, unsplit, ghost write
+    GhostedVector<double> u_gh = ghostRead(fineGrid, singleDof, u_vec);
+    GhostedVector<double> v_gh(fineGrid, singleDof);
+    // First subset --
+    std::vector<double> u_sub = ot::gather_ndofs(const_ptr(u_gh), subset, singleDof);
+    std::vector<double> v_sub(u_sub.size(), 0);
+        // matvec(): ghosted=true if created(DA), ghosted=false if created(subset)
+    amatFree[0]->matvec(ptr(v_sub), const_ptr(u_sub), false);
+    ot::scatter_ndofs_accumulate(const_ptr(v_sub), ptr(v_gh), subset, singleDof);
+    // -- If there was a second subset, do the same with it
+    //
+    LocalVector<double> v_vec = ghostWriteAccumulate(fineGrid, singleDof, v_gh);
+    /// printLocal(fineGrid.da(), const_ptr(v_vec));
+
+    LocalVector<double> residual = v_vec - rhs_vec;
+
+    using Poisson = PoissonEq::PoissonMat<DIM>;
+
+    /// Restriction restriction(fineGrid, surrGrid[1], grid[1]);  // Fine(0)
+    /// Prolongation prolongation(grid[1], surrGrid[1], fineGrid);
+    /// Operator<Poisson> coarse_op = galerkinCoarseOp(fine_op, restriction, prolongation);
 
 
-      /// printLocal(fineGrid.da(), const_ptr(v_vec));
+    /// LocalVector<double> rhs_2h = restriction(residual, fineGrid, surrGrid[1], grid[1]);
 
-      //TODO
-      /// amatWrapper.stMatFree->apply_bc(Mfrhs.vec());
+    /// LocalVector<double> err_2h = solve_2h(grid[1], rhs_2h); //TODO need operators..
 
-  std::cout << "dtree number of grids == " << dtree.getNumStrata() << "\n";
-  std::cout << "fine grid number of elements == " << multiDA[0].getGlobalElementSz() << "\n";
+    /// LocalVector<double> err_h = prolongation(err_2h, grid[1], surrGrid[1], fineGrid);
+
+    /// u_vec = u_vec - err_h;
+  }
+
+  printf("[cycle=%2d]  |error|=%.2e\n",
+      nvcycle, sol_err_linf(u_vec));
 
   _DestroyHcurve();
   DendroScopeEnd();
@@ -361,6 +491,35 @@ LocalVector<DT> ghostWriteOverwrite(const ot::GridWrapper<dim> &grid, const int 
   return ot::makeLocal(grid, std::move(ghCopy));
 }
 
+
+// boundaryFlags()
+template <typename C, unsigned int dim>
+std::vector<bool> boundaryFlags(
+    const ot::DistTree<C, dim> &distTree,
+    const int stratum)
+{
+  const std::vector<ot::TreeNode<C, dim>> &elements =
+      distTree.getTreePartFiltered(stratum);
+  const size_t size = elements.size();
+
+  std::vector<bool> boundaryFlags(size, false);
+
+  for (size_t ii = 0; ii < size; ++ii)
+    boundaryFlags[ii] = elements[ii].getIsOnTreeBdry();
+
+  return boundaryFlags;
+}
+
+
+// subsetBoundary()
+ot::LocalSubset<DIM> subsetBoundary(const GridWrapper &grid)
+{
+  return ot::LocalSubset<DIM>(
+      grid.da(),
+      &grid.octList(),
+      boundaryFlags(*grid.distTree(), grid.stratum()),
+      true);
+}
 
 // subsetAll()
 ot::LocalSubset<DIM> subsetAll(const GridWrapper &grid)
