a70d159831ff5a49e8967a12c4bf28b224c0cca4
Masado Ishii <masado.ishii@gmail.com>
2022-02-16 15:39:35 -0700

    Delete some unused code

--- include/tsort.h
+++ include/tsort.h
@@ -121,73 +121,6 @@ SFC_State<dim> SFC_State<dim>::subcurve(sfc::SubIndex i) const
 
 
 
-//
-// BucketInfo{}
-//
-// Buckets to temporarily represent (interior) nodes in the hyperoctree
-// while we carry out breadth-first traversal. See distTreeSort().
-template <typename T>
-struct BucketInfo          // Adapted from Dendro4 sfcSort.h:132.
-{
-  sfc::RotIndex rot;
-  LevI lev;
-  T begin;
-  T end;
-
-  static BucketInfo root(T size)
-  {
-    return BucketInfo{ sfc::RotIndex(0), 0, 0, size};
-  }
-};
-
-template struct BucketInfo<RankI>;
-
-
-// Wrapper around std::vector to act like a queue, plus it has a
-// single barrier, which can be moved to the end of the queue at any time.
-// TODO This should go in some other utilities header file.
-template <typename T>
-struct BarrierQueue
-{
-  // Usage:
-  //
-  // BarrierQueue q;
-  // for (int i = 0; i < 5; i++) { q.enqueue(i); }
-  // q.reset_barrier();
-  // for (int i = 5; i < 10; i++) { q.enqueue(i); }
-  //
-  // int x;
-  // while (q.dequeue(x)) { std::cout << x << ' '; }  // 0 1 2 3 4
-  // q.reset_barrier();
-  // while (q.dequeue(x)) { std::cout << x << ' '; }  // 5 6 7 8 9
-
-  struct Range
-  {
-    typename std::vector<T>::iterator m_begin, m_end;
-    typename std::vector<T>::iterator begin() { return m_begin; }
-    typename std::vector<T>::iterator end() { return m_end; }
-  };
-
-  typename std::vector<T>::size_type b;  // An out-of-band barrier.
-  std::vector<T> q;             // If you modify this, call reset_barrier() afterward.
-
-  BarrierQueue() : q(), b(0) {};
-  BarrierQueue(typename std::vector<T>::size_type s) : q(s), b(0) {};
-  BarrierQueue(typename std::vector<T>::size_type s, T val) : q(s, val), b(0) {};
-  void clear() { q.clear(); b = 0; }
-  void reset_barrier() { b = q.size(); }
-  void resize_back(typename std::vector<T>::size_type count) { q.resize(count + b); }
-  typename std::vector<T>::size_type get_barrier() { return b; }
-  typename std::vector<T>::size_type size() { return q.size(); }
-  T front() { return *q.begin(); }
-  T back() { return *q.end(); }
-  Range leading() { return {q.begin(), q.begin() + b}; }
-  Range trailing() { return {q.begin() + b, q.end()}; }
-  void enqueue(T val) { q.push_back(val); }
-  typename std::vector<T>::size_type dequeue(T &val)
-  { if (b > 0) { val = q[0]; q.erase(q.begin()); } return (b > 0 ? b-- : 0); }
-};
-
 template <typename T, unsigned int dim>
 struct KeyFunIdentity_TN
 {
@@ -563,17 +496,6 @@ struct SFC_Tree
                            unsigned int noSplitThresh,
                            double loadFlexibility,
                            MPI_Comm comm);
-
-  //
-  // treeBFTNextLevel()
-  //   Takes the queue of BucketInfo in a breadth-first traversal, and finishes
-  //   processing the current level. Each dequeued bucket is subdivided,
-  //   and the sub-buckets in the corresponding range of `points` are sorted.
-  //   Then the sub-buckets are initialized and enqueued to the back.
-  //
-  static void treeBFTNextLevel(TreeNode<T,dim> *points,
-      std::vector<BucketInfo<RankI>> &bftQueue);
-
 
   /**
    * @brief Broadcast the first TreeNode from every processor so we have global access to the splitter list.
