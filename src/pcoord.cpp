a43c9bbab65ceb4ceb747b54fde21ea17d1e585a
Masado Ishii <masado.ishii@gmail.com>
2022-03-09 14:19:35 -0700

    Revert "Use largest type of unsigned -1 for default mask corresponding to NO_PERIOD"
    This reverts commit abf3bef8874cba283bf35b1aee6d8d6825748329.
    
    Long long unsigned to unsigned is a narrowing conversion.

--- src/pcoord.cpp
+++ src/pcoord.cpp
@@ -7,7 +7,7 @@ namespace periodic
   template class PCoord<unsigned, 3>;
   template class PCoord<unsigned, 4>;
 
-  template <> std::array<unsigned, 2> PCoord<unsigned, 2>::m_masks = {-1llu, -1llu};
-  template <> std::array<unsigned, 3> PCoord<unsigned, 3>::m_masks = {-1llu, -1llu, -1llu};
-  template <> std::array<unsigned, 4> PCoord<unsigned, 4>::m_masks = {-1llu, -1llu, -1llu, -1llu};
+  template <> std::array<unsigned, 2> PCoord<unsigned, 2>::m_masks = {-1u, -1u};
+  template <> std::array<unsigned, 3> PCoord<unsigned, 3>::m_masks = {-1u, -1u, -1u};
+  template <> std::array<unsigned, 4> PCoord<unsigned, 4>::m_masks = {-1u, -1u, -1u, -1u};
 }
