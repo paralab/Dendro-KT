dd5a65ddbd3a222b8dc2c3972b99a25683366dd1
Masado Ishii <masado.ishii@gmail.com>
2022-10-21 16:56:49 -0600

    (wip) Add mg Jacobi smoother, restriction, setup in driver, and use mg as preconditioner in cg
    next is to add prolongation and coarse grid solver.
    
    right now the restriction is calculated but not used.
    therefore the effect of the vcycle is 2 iterations of damped jacobi,
    so we can see the effect of jacobi preconditioning of cg
    (it reduced the number of iterations by a small percentage).

--- FEM/examples/src/doctest_gmg_poisson.cpp
+++ FEM/examples/src/doctest_gmg_poisson.cpp
@@ -18,6 +18,18 @@
 #include <vector>
 #include <functional>
 
+
+// -----------------------------
+// TODO
+// ✔ Define VectorPool member functions
+// ✔ Define smoother (start with Jacobi smoother)
+// ✔ Define restriction (adapt from gmgMat)
+// _ Define prolongation (follow restriction, adapt from gmgMat)
+// _ Finish vcycle() definition
+// _ Refactor MatVec to wrap a new MatVecGhosted, which can be called in the solver
+// _ future: 4th kind Chebyshev smoother
+// -----------------------------
+
 // -----------------------------
 // Typedefs
 // -----------------------------
@@ -32,12 +44,201 @@ std::ostream & print(const ot::DA<dim> &da,
                      const std::vector<NodeT> &local_vec,
                      std::ostream & out = std::cerr);
 
+// -----------------------------
+// Structs
+// -----------------------------
+
+// DA_Pair
+template <int dim>
+struct DA_Pair
+{
+  const ot::DA<dim> *primary;
+  const ot::DA<dim> *surrogate;
+};
+
+#include <map>
+
+template <typename X>
+class VectorPool
+{
+  public:
+    VectorPool() = default;
+
+    ~VectorPool() { std::cerr << "checkouts==" << m_log_checkouts << " \tallocs==" << m_log_allocs << "\n"; }
+
+    void free_all();
+    std::vector<X> checkout(size_t size);
+    void checkin(std::vector<X> &&vec);
+      //idea: multimap find, assign moved, erase empty, return assigned; if not found, return new.
+
+    VectorPool(VectorPool && other) : m_idle(std::move(other.m_idle)) {}
+    VectorPool & operator=(VectorPool &&other) { m_idle = std::move(other.m_idle); return *this; }
+    VectorPool(const VectorPool &) = delete;
+    VectorPool & operator=(const VectorPool &) = delete;
+
+  private:
+    std::multimap<int, std::vector<X>> m_idle;
+    long long unsigned m_log_checkouts = 0;
+    long long unsigned m_log_allocs = 0;
+};
+
+template <typename X>
+void VectorPool<X>::free_all()
+{
+  m_idle.clear();
+}
+
+template <typename X>
+std::vector<X> VectorPool<X>::checkout(size_t size)
+{
+  ++m_log_checkouts;
+  auto it = m_idle.find(size);
+  if (it == m_idle.end())
+  {
+    ++m_log_allocs;
+    return std::vector<X>(size);
+  }
+  std::vector<X> extracted = std::move(it->second);
+  m_idle.erase(it);
+  return extracted;
+}
+
+template <typename X>
+void VectorPool<X>::checkin(std::vector<X> &&vec)
+{
+  m_idle.insert({vec.size(), std::move(vec)});
+}
+
+// -----------------------------
+// Functions
+// -----------------------------
+
 // cgSolver()
 template <unsigned int dim, typename MatMult>
 int cgSolver(const ot::DA<dim> *da, const MatMult &mat_mult, VECType * u, const VECType * rhs, int max_iter, double relResErr);
 
+// matvec_base_in()
+template <typename X, unsigned dim>
+ot::MatvecBaseIn<dim, X> matvec_base_in(
+    const ot::DA<dim> *da, int ndofs, const X *ghosted, int extra_depth = 0);
+
+// matvec_base_ou_accumulatet()
+template <typename X, unsigned dim>
+ot::MatvecBaseOut<dim, X, true> matvec_base_out_accumulate(
+    const ot::DA<dim> *da, int ndofs, int extra_depth = 0);
+
+// restrict_fine_to_coarse()
+template <int dim>
+void restrict_fine_to_coarse(
+    DA_Pair<dim> fine_da, const VECType *fine_vec_ghosted,
+    DA_Pair<dim> coarse_da, VECType *coarse_vec_ghosted,
+    int ndofs,
+    VectorPool<VECType> &vector_pool)
+{
+  const unsigned int nPe = fine_da.primary->getNumNodesPerElement();
+
+  std::vector<VECType> coarse_surr_ghosted = vector_pool.checkout(
+      coarse_da.surrogate->getTotalNodalSz() * ndofs);
+
+  std::vector<VECType> leafBuffer = vector_pool.checkout(ndofs * nPe);
+  leafBuffer.assign(leafBuffer.size(), 42);
+
+  // Surrogate is coarse grid partitioned by fine
+  // Interpolate^T from the fine primary grid to coarse surrogate.
+
+  // readFromGhost*(fine_vec_ghosted) must precede restrict_fine_to_coarse().
+
+  // Fine ghosted elemental owners.
+  using OwnershipT = DendroIntL;
+  const OwnershipT * ownersGhostedPtr = fine_da.primary->getNodeOwnerElements();
+
+  if (fine_da.primary->getLocalNodalSz() > 0)
+  {
+    // Index fine grid elements as we loop.
+    OwnershipT globElementId = fine_da.primary->getGlobalElementBegin();
+
+    unsigned eleOrder = fine_da.primary->getElementOrder();
+
+    // Fine and coarse element-to-node loops.
+    ot::MatvecBaseIn<dim, OwnershipT>
+        loopOwners = matvec_base_in(fine_da.primary, ndofs, ownersGhostedPtr, 0);
+    ot::MatvecBaseIn<dim, VECType>
+        loopFine = matvec_base_in(fine_da.primary, ndofs, fine_vec_ghosted, 0);
+    ot::MatvecBaseOut<dim, VECType, true>
+        loopCoarse = matvec_base_out_accumulate<VECType>(coarse_da.surrogate, ndofs, 1);
+
+    // Traverse fine and coarse grids simultaneously.
+    while (!loopFine.isFinished())
+    {
+      // Depth controlled by fine.
+      if (loopFine.isPre() && loopFine.subtreeInfo().isLeaf())
+      {
+        const VECType * fineLeafIn = loopFine.subtreeInfo().readNodeValsIn();
+        const OwnershipT * fineOwners = loopOwners.subtreeInfo().readNodeValsIn();
+        for (size_t nIdx = 0; nIdx < nPe; ++nIdx)
+        {
+          if (loopFine.subtreeInfo().readNodeNonhangingIn()[nIdx])
+          {
+            // Only transfer a node to parent from the owning element.
+            if (fineOwners[nIdx] == globElementId)
+            {
+              for (int dof = 0; dof < ndofs; ++dof)
+                leafBuffer[ndofs * nIdx + dof] = fineLeafIn[ndofs * nIdx + dof];
+            }
+            else
+            {
+              for (int dof = 0; dof < ndofs; ++dof)
+                leafBuffer[ndofs * nIdx + dof] = 0;
+            }
+          }
+          else
+          {
+            for (int dof = 0; dof < ndofs; ++dof)
+              leafBuffer[ndofs * nIdx + dof] = 0.0f;
+          }
+        }
+
+        loopCoarse.subtreeInfo().overwriteNodeValsOut(leafBuffer.data());
+
+        loopFine.next();
+        loopCoarse.next();
+        loopOwners.next();
+
+        globElementId++;
+      }
+      else
+      {
+        loopFine.step();
+        loopCoarse.step();
+        loopOwners.step();
+      }
+    }
+    const size_t writtenSz = loopCoarse.finalize(coarse_surr_ghosted.data());
+  }
+
+  // Coarse ghost write.
+  coarse_da.surrogate->writeToGhostsBegin(coarse_surr_ghosted.data(), ndofs);
+  coarse_da.surrogate->writeToGhostsEnd(coarse_surr_ghosted.data(), ndofs);
+
+  // Shift in the coarse grid from surrogate to primary.
+  ot::distShiftNodes(
+      *coarse_da.surrogate,
+      coarse_surr_ghosted.data() + coarse_da.surrogate->getLocalNodeBegin() * ndofs,
+      *coarse_da.primary,
+      coarse_vec_ghosted + coarse_da.primary->getLocalNodeBegin() * ndofs,
+      ndofs);
+
+  vector_pool.checkin(std::move(leafBuffer));
+  vector_pool.checkin(std::move(coarse_surr_ghosted));
+}
+
+// prolongate_coarse_to_fine()
+    /// VectorPool &vector_pool)
 
 
+// -----------------------------
+// Main test cases
+// -----------------------------
 
 MPI_TEST_CASE("Poisson problem on a uniformly refined cube with 5 processes, should converge to exact solution", 5)
 {
@@ -118,12 +319,15 @@ MPI_TEST_CASE("Poisson problem on a uniformly refined cube with 5 processes, sho
   std::vector<double> f_vec = local_vector(base_da, double{}, single_dof);
   std::vector<double> rhs_vec = local_vector(base_da, double{}, single_dof);
 
+  using PoissonMat = PoissonEq::PoissonMat<dim>;
+
   // fe_matrix()
-  const auto fe_matrix = [&](const ot::DA<dim> &da) -> PoissonEq::PoissonMat<dim>
+  //  future: return PoissonMat without dynamic memory, after finish move constructors
+  const auto fe_matrix = [&](const ot::DA<dim> &da) -> PoissonMat *
   {
-    PoissonEq::PoissonMat<dim> mat(
+    auto *mat = new PoissonMat(
         &da, &da.dist_tree()->getTreePartFiltered(), single_dof);
-    mat.setProblemDimensions(min_corner, max_corner);
+    mat->setProblemDimensions(min_corner, max_corner);
     return mat;
   };
 
@@ -149,7 +353,7 @@ MPI_TEST_CASE("Poisson problem on a uniformly refined cube with 5 processes, sho
     f_vec[i] = f(&local_node_coordinate(base_da, i).x(0));
 
   // Linear system oracles encapsulating the operators.
-  PoissonEq::PoissonMat<dim> base_mat = fe_matrix(base_da);
+  PoissonEq::PoissonMat<dim> &base_mat = *fe_matrix(base_da);  //future: not a pointer
   PoissonEq::PoissonVec<dim> base_vec = fe_vector(base_da);
 
   // Compute right hand side, subtracting boundary data.
@@ -174,14 +378,158 @@ MPI_TEST_CASE("Poisson problem on a uniformly refined cube with 5 processes, sho
       return err_max;
   };
 
+  // Multigrid setup
+  //  future: distCoarsen to coarsen by 1 or more levels
+  //  future: coarse_to_fine and fine_to_coarse without surrogates
+  //  future: Can do 2:1-balance all-at-once instead of iteratively.
+  const int n_grids = 2;
+  ot::DistTree<uint, dim> &trees = base_tree;
+  ot::DistTree<uint, dim> surrogate_trees;
+  surrogate_trees = trees.generateGridHierarchyUp(true, n_grids, partition_tolerance);
+  assert(trees.getNumStrata() == n_grids);
+  /// struct DA_Pair { const ot::DA<dim> *primary; const ot::DA<dim> *surrogate; };
+  std::vector<DA_Pair<dim>> das(n_grids, DA_Pair<dim>{nullptr, nullptr});
+  das[0].primary = &base_da;
+  for (int g = 1; g < n_grids; ++g)
+  {
+    das[g].primary = new ot::DA<dim>(
+        trees, g, comm, polynomial_degree, size_t{}, partition_tolerance);
+    das[g].surrogate = new ot::DA<dim>(
+        surrogate_trees, g, comm, polynomial_degree, size_t{}, partition_tolerance);
+  }
+  std::vector<std::vector<double>> u_ghosted(n_grids);
+  std::vector<std::vector<double>> r_ghosted(n_grids);
+  for (int g = 0; g < n_grids; ++g)
+  {
+    u_ghosted[g].resize(das[g].primary->getTotalNodalSz() * single_dof);
+    r_ghosted[g].resize(das[g].primary->getTotalNodalSz() * single_dof);
+  }
+
+  std::vector<PoissonMat *> mats(n_grids, nullptr);
+  mats[0] = &base_mat;
+  for (int g = 1; g < n_grids; ++g)
+    mats[g] = fe_matrix(*das[g].primary);
+
+  std::vector<std::vector<double>> a_diag_ghosted(n_grids);
+  for (int g = 0; g < n_grids; ++g)
+  {
+    a_diag_ghosted[g].resize(das[g].primary->getTotalNodalSz() * single_dof);
+    mats[g]->setDiag(a_diag_ghosted[g].data() + das[g].primary->getLocalNodeBegin() * single_dof);
+    das[g].primary->readFromGhostBegin(a_diag_ghosted[g].data(), single_dof);
+    das[g].primary->readFromGhostEnd(a_diag_ghosted[g].data(), single_dof);
+  }
+
+  VectorPool<double> vector_pool;
+
+  // Jacobi relaxation (one iteration)
+  const auto jacobi = [&](PoissonMat *mat, double *u_ghost, double *r_ghost, const double damp, const double *diag_ghosted) -> void
+  {
+    const ot::DA<dim> *da = mat->da();
+    std::vector<double> Dinv_r = vector_pool.checkout(da->getTotalNodalSz() * single_dof);
+
+    for (size_t i = 0; i < da->getTotalNodalSz() * single_dof; ++i)
+    {
+      const double update = damp / diag_ghosted[i] * r_ghost[i];
+      Dinv_r[i] = update;
+      u_ghost[i] += update;
+    }
+
+    // future: matVecGhosted directly on fresh ghosted data
+    mat->matVec(
+        Dinv_r.data() + da->getLocalNodeBegin() * single_dof,
+        Dinv_r.data() + da->getLocalNodeBegin() * single_dof);
+    auto & ADinv_r = Dinv_r;
+    da->readFromGhostBegin(ADinv_r.data(), single_dof);
+    da->readFromGhostEnd(ADinv_r.data(), single_dof);
+
+    for (size_t i = 0; i < da->getTotalNodalSz() * single_dof; ++i)
+      r_ghost[i] -= ADinv_r[i];
+
+    vector_pool.checkin(std::move(Dinv_r));
+  };
+
+  // Multigrid v-cycle
+  //  Parameters: [in-out] u  initial guess and final solution
+  //              [in-out] r  residual of initial guess and of final solution
+  const auto vcycle = [&](double *u, double *r) -> void
+  {
+    // Copy u, r to u_ghosted, r_ghosted.
+    base_da.nodalVecToGhostedNodal(r, r_ghosted[0].data(), single_dof);
+    base_da.nodalVecToGhostedNodal(u, u_ghosted[0].data(), single_dof);
+    //
+    base_da.readFromGhostBegin(r_ghosted[0].data(), single_dof);
+    base_da.readFromGhostBegin(u_ghosted[0].data(), single_dof);
+    base_da.readFromGhostEnd(r_ghosted[0].data(), single_dof);
+    base_da.readFromGhostEnd(u_ghosted[0].data(), single_dof);
+
+    const double damp = 2.0/3.0;
+
+    for (int height = 0; height < n_grids - 1; ++height)
+    {
+      // pre-smoothing (on ghosted vectors)
+      jacobi(mats[height], u_ghosted[height].data(), r_ghosted[height].data(), damp, a_diag_ghosted[height].data());
+
+      // restriction (fine-to-coarse) (on ghosted vectors)
+      restrict_fine_to_coarse(
+          das[height], r_ghosted[height].data(),
+          das[height+1], r_ghosted[height+1].data(),
+          single_dof, vector_pool);
+    }
+
+    // Coarse solve
+
+    for (int height = n_grids - 1; height > 0; --height)
+    {
+      // prolongation (coarse-to-fine) (on ghosted vectors)
+
+      // post-smoothing (on ghosted vectors)
+      jacobi(mats[height-1], u_ghosted[height-1].data(), r_ghosted[height-1].data(), damp, a_diag_ghosted[height-1].data());
+    }
+
+    // Copy u_ghosted, r_ghosted to u, r.
+    base_da.ghostedNodalToNodalVec(r_ghosted[0].data(), r, single_dof);
+    base_da.ghostedNodalToNodalVec(u_ghosted[0].data(), u, single_dof);
+  };
+
+  // Preconditioned right-hand side.
+  std::vector<double> pc_rhs_vec = rhs_vec;
+  std::vector<double> v_temporary = pc_rhs_vec;
+  pc_rhs_vec.assign(pc_rhs_vec.size(), 0);  // reset to zero
+  vcycle(pc_rhs_vec.data(), v_temporary.data());
+
+  // Preconditioned matrix multiplication.
+  const auto pc_mat = [&base_mat, &vcycle, &v_temporary](const double *u, double *v) -> void {
+    base_mat.matVec(u, v_temporary.data());
+
+    std::fill_n(v, v_temporary.size(), 0); // reset to zero
+    vcycle(v, v_temporary.data());
+  };
+
+  // Solve equation.
   const double tol=1e-14;
   const unsigned int max_iter=1500;
 
   double relative_residual = tol;
+  /// const int steps = cgSolver(
+  ///     &base_da, [&base_mat](const double *u, double *v){ base_mat.matVec(u, v); },
+  ///     &(*u_vec.begin()), &(*rhs_vec.begin()), max_iter, relative_residual);
   const int steps = cgSolver(
-      &base_da, [&base_mat](const double *u, double *v){ base_mat.matVec(u, v); },
-      &(*u_vec.begin()), &(*rhs_vec.begin()), max_iter, relative_residual);
+      &base_da, pc_mat,
+      &(*u_vec.begin()), &(*pc_rhs_vec.begin()), max_iter, relative_residual);
   const double err = sol_err_max(u_vec);
+
+  // Multigrid teardown.
+  for (int g = 1; g < n_grids; ++g)
+  {
+    delete das[g].primary;
+    delete das[g].surrogate;
+    delete mats[g];
+  }
+  das.clear();
+  mats.clear();
+
+  // future: base_mat can stay in stack memory, don't need to new/delete
+  delete &base_mat;
 
   if(!rank)
   {
@@ -306,5 +654,36 @@ int cgSolver(const ot::DA<dim> *da, const MatMult &mat_mult, VECType * u, const 
   return step;
 }
 
+
+// matvec_base_in()
+template <typename X, unsigned dim>
+ot::MatvecBaseIn<dim, X> matvec_base_in(
+    const ot::DA<dim> *da, int ndofs, const X *ghosted, int extra_depth)
+{
+  return ot::MatvecBaseIn<dim, X>(
+        da->getTotalNodalSz(), ndofs, da->getElementOrder(),
+        extra_depth > 0, extra_depth,
+        da->getTNCoords(), ghosted,
+        da->dist_tree()->getTreePartFiltered(da->stratum()).data(),
+        da->dist_tree()->getTreePartFiltered(da->stratum()).size(),
+        *da->getTreePartFront(),
+        *da->getTreePartBack());
+}
+
+// matvec_base_ou_accumulatet()
+template <typename X, unsigned dim>
+ot::MatvecBaseOut<dim, X, true> matvec_base_out_accumulate(
+    const ot::DA<dim> *da, int ndofs, int extra_depth)
+{
+  const bool accumulate = true;
+  return ot::MatvecBaseOut<dim, X, accumulate>(
+      da->getTotalNodalSz(), ndofs, da->getElementOrder(),
+      extra_depth > 0, extra_depth,
+      da->getTNCoords(),
+      da->dist_tree()->getTreePartFiltered(da->stratum()).data(),
+      da->dist_tree()->getTreePartFiltered(da->stratum()).size(),
+      *da->getTreePartFront(),
+      *da->getTreePartBack());
+}
 
 
